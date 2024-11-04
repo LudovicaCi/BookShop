335286bc3553db3a67a29aa7f247fd46580fefe4
Ludovica Cocchella <84328863+LudovicaCi@users.noreply.github.com>
2024-11-04 22:27:32 +0100

    Upload frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookTab from './components/BookTab';
+
+const App = () => {
+  return (
+      <div>
+        <BookTab />
+      </div>
+  );
+};
+
+export default App;
